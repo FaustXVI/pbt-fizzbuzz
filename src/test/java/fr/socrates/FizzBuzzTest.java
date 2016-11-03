937769bb0b8957d8a7d44c95e1c29bd3d84620e7
FaustXVI <xavier.detant@gmail.fr>
2016-11-03 11:59:32 +0100

    1 should be translated to "1"
    This is our first red test: it doesn't compile.
    Let's make it compile first and see the error message.

---
+++ src/test/java/fr/socrates/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package fr.socrates;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void shouldReturnOneGivenOne() {
+        assertThat(fizzBuzz(1)).isEqualTo("1");
+    }
+}
