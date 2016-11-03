26956220519432c416c623ad46284ba0e3b9bf02
FaustXVI <xavier.detant@gmail.fr>
2016-11-03 14:08:35 +0100

    Returns "Fizz" given 9
    All tests are green and we clearly see the duplication of the concept of Fizz.
    Let's refactor this !

--- src/test/java/fr/socrates/FizzBuzzTest.java
+++ src/test/java/fr/socrates/FizzBuzzTest.java
@@ -49,6 +49,7 @@ public class FizzBuzzTest {
     private String fizzBuzz(int n) {
         return n == 3 ? "Fizz"
                 : n == 6 ? "Fizz"
+                : n == 9 ? "Fizz"
                 : String.valueOf(n);
     }
 }
